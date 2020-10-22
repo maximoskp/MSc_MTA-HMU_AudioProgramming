c1c1997e5775c3b8ae3822385333ee2d1fb74701
maximoskp <maximos@athenarc.gr>
2020-10-22 21:05:05 +0300

    Example for Nyquist frequency

--- class2_sound.py
+++ class2_sound.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sounddevice as sd
 
-sr = 44100
+sr = 1000
 
 t = np.arange(sr)/sr
 
@@ -18,11 +18,11 @@ t = np.arange(sr)/sr
 # s = np.power( t, 2 )
 
 # frequency
-f = 3
+f = 250 # check aliasing, e.g. f = 750
 # amplitude
 a = 0.8
 # phase
-p = 0.25
+p = 0
 
 s = a*np.sin( 2*np.pi*f*t + p*2*np.pi )
 
