6a777c309547be84710e0989a127c652f5fc14ca
maximoskp <maximoskalpap@gmail.com>
2023-11-27 14:51:50 +0200

    before cpp 2nd class

--- class06/cpp_introduction/01_hello.cpp
+++ class06/cpp_introduction/01_hello.cpp
@@ -2,8 +2,13 @@
 // how to run (assuming that the g++ compiler is available):
 // g++ -o hello 01_hello.cpp
 // Terminal shortcut on VSCode: ctrl+~
+/*
+This is a large comment.
+New lines are accepted.
+*/
 
-int main(){
+int main()
+{
     std::cout << "Hello World!" << std::endl << " - more stuff";
     // printf("with printf %s\n", "lala");
     return 0;
